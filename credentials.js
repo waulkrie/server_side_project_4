f064ded6aa2c2dcda3a42a99f8eed54f32654176
Andrew Bare <waulkrie@users.noreply.github.com>
2024-02-29 09:48:10 -0600

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -7,7 +7,7 @@ const creds = {
       connectionString: 'mongodb://localhost:27017' // Defaults to localhost, change if using Mongodb Atlas
     },
     production: {
-      connectionString: 'mongodb+srv://doadmin:aV3wc6078uCL9g14@db-mongodb-nyc1-tv-guide-app-2ee665f1.mongo.ondigitalocean.com'
+      
     },
   }
 };
